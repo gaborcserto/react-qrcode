dfe1303915c773123a0fe548c6dce52b09b025fc
Gabor Cserto <csertog@gmail.com>
2020-09-01 12:35:35 +0200

    Updates

---
+++ precache-manifest.0c1178fab6e517a0275880a797ed2259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd665c207ad4eb0966e8a00a41f2aff6",
+    "url": "/react-qrcode/index.html"
+  },
+  {
+    "revision": "eaa2c2f0cc5cef76f694",
+    "url": "/react-qrcode/static/css/main.50b4c46e.chunk.css"
+  },
+  {
+    "revision": "fa8ec17f3eddffcb27b6",
+    "url": "/react-qrcode/static/js/2.696ee2a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-qrcode/static/js/2.696ee2a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaa2c2f0cc5cef76f694",
+    "url": "/react-qrcode/static/js/main.264f343b.chunk.js"
+  },
+  {
+    "revision": "bf1db3db729ef55c4655",
+    "url": "/react-qrcode/static/js/runtime-main.1764750a.js"
+  }
+]);
