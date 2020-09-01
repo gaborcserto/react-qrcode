5757f8c1b1761c4e5fc83edf51a02dfed8dfeb31
Gabor Cserto <csertog@gmail.com>
2020-09-01 12:28:57 +0200

    implemented the qrcode

--- src/App.js
+++ src/App.js
@@ -4,18 +4,29 @@ import Header from './components/Header';
 import QrCode from './components/QrCode';
 
 function App() {
-	const [codeContent, setCodeContent] = useState(null);
+	const [codeContent, setCodeContent] = useState('Text here');
 
-	const HandleInput = e => {
+	const handleInputChange = e => {
 		e.preventDefault();
 		setCodeContent(e.target.value);
+	}
+
+	const handleInputReset = e => {
+		setCodeContent('');
 	}
 
 	return (
 		<div className="App">
 			<Header title="QR code"/>
 			<QrCode content={codeContent}/>
-			<input type="text" className="input" onChange={HandleInput}/>
+			<form>
+				<input
+					type="text"
+					className="input"
+					onChange={handleInputChange}
+					value={codeContent}/>
+				{codeContent ? <button className="reset" onClick={handleInputReset}>✖</button> : null}
+			</form>
 		</div>
 	);
 }
--- src/components/QrCode.js
+++ src/components/QrCode.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import './QrCode.scss'
-import { applyPolyfills, defineCustomElements } from '@deckdeckgo/qrcode/dist/loader';
-
-applyPolyfills().then(() => {
-	defineCustomElements(window);
-});
+import { QRNormal } from 'react-qrbtf'
 
 const qrCode = ({content}) => {
 
 	return (
 		<div className="qr-code" >
-			{content ?
-			<deckgo-qrcode content={content}>
-				<img slot="logo" src="../logo.svg" alt=""/>
-			</deckgo-qrcode>
-				: null }
-
+			{content ? <QRNormal value={content} /> : null }
 		</div>
-
 	);
 }
 
